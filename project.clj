38cabb8df79bb98cc731c59745df67c71cbc4cc2
Jeff Weiss <jweiss@redhat.com>
2011-01-25 10:33:49 -0500

    added xmlrpc client dep

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject gnome.ldtp "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "A clojure xmlrpc client for the GNOME LDTP UI driver library"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.apache.xmlrpc/xmlrpc-client "3.1.3"]])
--- test/gnome/ldtp/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns gnome.ldtp.test.core
-  (:use [gnome.ldtp.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
