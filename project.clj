143a2f108fd3202d2e1f39aefe9039fae2624475
jmolet <jmolet@redhat.com>
2012-04-12 10:54:04 -0400

    incrementing version to differentiate from 1.2 clojure

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject gnome.ldtp "1.0.0-SNAPSHOT"
+(defproject gnome.ldtp "1.1.0-SNAPSHOT"
   :description "A clojure xmlrpc client for the GNOME LDTP UI driver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [necessary-evil "2.0.0-SNAPSHOT"]
-                 ;[org.apache.xmlrpc/xmlrpc-client "3.1.3"]
                  ]
   ;:dev-dependencies [[swank-clojure "1.2.1"]]
   )
