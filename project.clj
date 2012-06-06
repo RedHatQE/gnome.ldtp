1e729d1673c505557f0fad0eefd00f71349c74ac
James Molet <jmolet@redhat.com>
2012-06-06 14:20:06 -0400

    adding exclusions due to bug in version ranging

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A clojure xmlrpc client for the GNOME LDTP UI driver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [necessary-evil "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]
+                 [necessary-evil "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  ]
   ;:dev-dependencies [[swank-clojure "1.2.1"]]
   )
