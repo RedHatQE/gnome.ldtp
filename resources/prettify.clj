68f0f68b1a65224188a8fd5e4db81805c1b5a9fb
James Molet <jmolet@redhat.com>
2012-12-05 16:37:42 -0500

    updating the ldtp api

--- resources/prettify.clj
+++ resources/prettify.clj
@@ -1,4 +1,9 @@
-(with-open [f (FileWriter. "/home/jweiss/workspace/gnome.ldtp/resources/ldtp_api_pretty.clj")]
+
+;Assuming ldtp_api_raw.clj is the unformatted file
+;this will spit the formatted version in ldtp_api_pretty.clj.
+;Note that ldtp_api.clj is the git controlled (and hopefully
+;latest) version of ldtp_api_pretty.clj.
+(with-open [f (FileWriter. "/home/jmolet/Projects/gnome.ldtp/resources/ldtp_api_pretty.clj")]
   (pprint (with-in-str
-            (slurp "/home/jweiss/workspace/gnome.ldtp/resources/ldtp_api.clj") (read))
+            (slurp "/home/jmolet/Projects/gnome.ldtp/resources/ldtp_api_raw.clj") (read))
           f))
