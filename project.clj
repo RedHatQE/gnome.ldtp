d973c1316bfa93d1ce4f98142db8f7529cb24f41
James Molet <jmolet@redhat.com>
2013-01-30 10:42:57 -0500

    updated to clojure 1.4 and ldtp3 api

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject gnome.ldtp "1.1.0-SNAPSHOT"
+(defproject gnome.ldtp "1.2.0-SNAPSHOT"
   :description "A clojure xmlrpc client for the GNOME LDTP UI driver library"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [necessary-evil "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
-                 ]
+                 [necessary-evil "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
   ;:dev-dependencies [[swank-clojure "1.2.1"]]
   )
