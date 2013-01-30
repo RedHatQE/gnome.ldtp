74f390a1f96bac93c7a1b92461d6cca34645e0fd
James Molet <jmolet@redhat.com>
2013-01-30 10:34:54 -0500

    updated prettify.clj to work as a standalone script

--- resources/prettify.clj
+++ resources/prettify.clj
@@ -3,7 +3,10 @@
 ;this will spit the formatted version in ldtp_api_pretty.clj.
 ;Note that ldtp_api.clj is the git controlled (and hopefully
 ;latest) version of ldtp_api_pretty.clj.
-(with-open [f (FileWriter. "/home/jmolet/Projects/gnome.ldtp/resources/ldtp_api_pretty.clj")]
+
+(use 'clojure.java.io)
+(use 'clojure.pprint)
+(with-open [f (writer "/home/jmolet/Projects/gnome.ldtp/resources/ldtp_api_pretty.clj")]
   (pprint (with-in-str
             (slurp "/home/jmolet/Projects/gnome.ldtp/resources/ldtp_api_raw.clj") (read))
           f))
