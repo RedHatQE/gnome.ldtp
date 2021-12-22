5de592851786e8cda3db5812239cf39d13654914
Jan Stavěl @ RedHat <jstavel@redhat.com>
2021-12-22 21:15:38 +0100

    RHSMQE-3342: fix for clojure 1.10 upgrade

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gnome.ldtp "1.2.1-SNAPSHOT"
+(defproject gnome.ldtp "1.2.4-SNAPSHOT"
   :description "A clojure xmlrpc client for the GNOME LDTP UI driver library"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [necessary-evil "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.logging "1.2.3"]
+                 [necessary-evil "2.0.2" :exclusions [org.clojure/clojure]]]
   )
